a41167fad9ab67df022c9fee59f8b10aac5b3288
shubaivan <shuba.ivan.vikt@gmail.com>
2026-04-29 15:20:00 +0300

    add nelmio api doc + Swagger UI at /api/doc

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Shared\SharedBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
